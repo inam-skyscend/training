2630ecc8691a6406793a374efafaeef48f1ee06a
Inamhusain Datardi <82588536+inam-skyscend@users.noreply.github.com>
2025-06-05 15:20:18 +0530

    add more command

--- test.py
+++ test.py
@@ -2,3 +2,11 @@ name = "Inam"
 age = 24
 print("My name is", name)
 print("I am", age, "years old")
+
+
+fruits = ["Apple", "Banana", "Cherry"]
+for fruit in fruits:
+    print(f"I like {fruit}")
+
+for i in range(5):
+    print("Number:", i)
